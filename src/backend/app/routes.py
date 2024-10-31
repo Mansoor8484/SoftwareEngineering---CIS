d1b15a5d7373e64f820b77536a7749761e248868
dan <hurwitzdans@gmail.com>
2024-10-31 10:53:50 -0400

    Connecting Routes with Controllers
    Signed-off-by: dan <hurwitzdans@gmail.com>

--- src/backend/app/routes.py
+++ src/backend/app/routes.py
@@ -1,12 +1,32 @@
 from flask import Blueprint, render_template
+from .controllers import register
 
 # Create a blueprint for the main routes
 main = Blueprint('main', __name__)
 
-@main.route('/index')
-def index():
-    return 'Index Page'
+@main.route('/')
+def dashboard():
+    return 'Dashboard - Template Coming SOOOOOOOON'
 
-# @main.route('/')
-# def home():
-#     return render_template('home.html')
+@main.route('/transactions')
+def transactions():
+    return 'Transactions - Template Coming SOOOOOOON'
+
+@main.route('/budgeting')
+def budgeting():
+    return 'Budgeting - Template Coming SOOOOOOON'
+
+@main.route('/contact')
+def contact():
+    return 'Contact - Template Coming SOOOOOOON'
+
+@main.route('/register', methods=['POST'])
+def register():
+    return register()  # Call the controller function
+
+
+
+# Later, when templates are ready
+# @main.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html', data=my_data)
